9d3e5f3582fc629ef815e3c12e2a80c336f12532
Alessandro Sciarrillo <alessandr.sciarrill2@studio.unibo.it>
2022-03-21 21:55:13 +0100

    Added Basic Tests

--- src/test/java/test/AppTest.java
+++ src/test/java/test/AppTest.java
@@ -1,5 +1,25 @@
 package test;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class AppTest {
+        
+        private boolean ok = true;
+        
+        @Test
+        public void testBasic1() {
+                assertEquals(42, Integer.sum(19, 23));
+        }
+        
+        @Test
+        public void testBasic2() {
+                assertEquals(42, Integer.sum(19, 23));
+        }
+        
+        @Test
+        public void testBasic3() {
+                assertEquals(42, Integer.sum(19, 23));
+        }
 
 }
