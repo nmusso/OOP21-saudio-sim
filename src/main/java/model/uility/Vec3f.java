810a80df9defec1b662a43dd29c0a454493151b9
alex presepi <alex.presepi@studio.unibo.it>
2022-03-21 15:44:22 +0100

    Create vector utility interface

---
+++ src/main/java/model/uility/Vec3f.java
@@ -0,0 +1,19 @@
+package model.uility;
+
+public interface Vec3f {
+    
+    void setX(final double x);
+    
+    void setY(final double y);
+    
+    void setZ(final double z);
+    
+    double getX();
+    
+    double getY();
+    
+    double getZ();
+    
+    
+    
+}
