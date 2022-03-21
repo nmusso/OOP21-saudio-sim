8d8b8f5e8bb7ca544fd775602f44ec43c018a24f
alex presepi <alex.presepi@studio.unibo.it>
2022-03-21 15:45:14 +0100

    Create simple listener interface

---
+++ src/main/java/model/listener/SimpleListener.java
@@ -0,0 +1,39 @@
+package model.listener;
+
+import model.uility.Vec3f;
+
+public interface SimpleListener {
+    
+    /**
+     * Set the position (x,y,z) of the listener.
+     * @param pos vector of 3 float that representing x,y,z value.
+     */
+    void setPosition(final Vec3f pos);
+    
+    /**
+     * 
+     * @return vector of 3 float that representing x,y,z value.
+     */
+    Vec3f getPosition();
+    
+    /**
+     * 
+     * @param at
+     * @param up
+     */    
+    void setOrientation(final Vec3f at, final Vec3f up);
+    
+    /**
+     * 
+     * @return at vector 
+     */
+    Vec3f getAtOrientation();
+    
+    /**
+     *  
+     * @return up vector
+     */
+    Vec3f getUpOrientation();
+    
+
+}
