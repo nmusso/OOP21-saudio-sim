28ef5ddc4156c470c7ccde4d5a7bfd9f9b78c83f
alex presepi <alex.presepi@studio.unibo.it>
2022-03-29 22:33:42 +0200

    Fix ListenerImpl

--- src/main/java/model/listener/ListenerImpl.java
+++ src/main/java/model/listener/ListenerImpl.java
@@ -5,8 +5,7 @@ import model.utility.Vec3f;
 import org.lwjgl.openal.AL10;
 import static org.lwjgl.openal.AL10.alListener3f;
 import static org.lwjgl.openal.AL10.alListenerfv;
-
-
+import java.util.Objects;
 
 public class ListenerImpl implements Listener {
 
@@ -15,18 +14,16 @@ public class ListenerImpl implements Listener {
     private Vec3f atOrientation;
     private Vec3f upOrientation;
 
-
-
     ListenerImpl(final Context context) {
         this.context = context;
         this.setPosition(new Vec3f(0.0f, 0.0f, 0.0f));
-        this.setOrientation(new Vec3f(0.0f, 1.0f, 0.0f), new Vec3f(0.0f, 0.0f, 1.0f));
+        this.setOrientation(new Vec3f(0.0f, 0.0f, -1.0f), new Vec3f(0.0f, 1.0f, 0.0f));
     }
 
     ListenerImpl(final Context context, final Vec3f position) {
         this.context = context;
         this.setPosition(position);
-        this.setOrientation(new Vec3f(0.0f, 1.0f, 0.0f), new Vec3f(0.0f, 0.0f, 1.0f));
+        this.setOrientation(new Vec3f(0.0f, 0.0f, -1.0f), new Vec3f(0.0f, 1.0f, 0.0f));
     }
 
     ListenerImpl(final Context context, final Vec3f position, final Vec3f at, final Vec3f up) {
@@ -35,11 +32,10 @@ public class ListenerImpl implements Listener {
         this.setOrientation(at, up);
     }
 
-
     /**
-    * 
-    *@inheritDoc
-    */
+     * 
+     * @inheritDoc
+     */
     @Override
     public final void setPosition(final Vec3f pos) {
         this.position = pos;
@@ -48,7 +44,7 @@ public class ListenerImpl implements Listener {
 
     /**
      * 
-     *@inheritDoc
+     * @inheritDoc
      */
     @Override
     public Vec3f getPosition() {
@@ -57,21 +53,21 @@ public class ListenerImpl implements Listener {
 
     /**
      * 
-     *@inheritDoc
+     * @inheritDoc
      */
     @Override
     public final void setOrientation(final Vec3f at, final Vec3f up) {
-        this.atOrientation = at;
-        this.upOrientation = up;
+        this.checkLinearlyInd(at, up);
 
-        final float[] atUpVec = {this.atOrientation.getX(), this.atOrientation.getY(), this.atOrientation.getZ(), this.upOrientation.getX(), this.upOrientation.getY(), this.upOrientation.getZ()};
+        final float[] atUpVec = { this.atOrientation.getX(), this.atOrientation.getY(), this.atOrientation.getZ(),
+                this.upOrientation.getX(), this.upOrientation.getY(), this.upOrientation.getZ() };
         alListenerfv(AL10.AL_ORIENTATION, atUpVec);
 
     }
 
     /**
      * 
-     *@inheritDoc
+     * @inheritDoc
      */
     @Override
     public Vec3f getAtOrientation() {
@@ -80,23 +76,56 @@ public class ListenerImpl implements Listener {
 
     /**
      * 
-     *@inheritDoc
+     * @inheritDoc
      */
     @Override
     public Vec3f getUpOrientation() {
         return this.upOrientation;
     }
 
-
     /**
      * 
-     *@inheritDoc
+     * @inheritDoc
      */
     @Override
     public Context getCurrentContext() {
         return this.context;
     }
 
+    private boolean checkLinearlyInd(final Vec3f at, final Vec3f up) {
+        /* TODO */
+        this.atOrientation = at;
+        this.upOrientation = up;
+        return true;
+    }
+
+    /**
+     * 
+     * @inheritDoc
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(atOrientation, context, position, upOrientation);
+    }
+
+    /**
+     * 
+     * @inheritDoc
+     */
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+
+        final ListenerImpl other = (ListenerImpl) obj;
+        return context.equals(other.getCurrentContext());
+    }
 
 }
-
