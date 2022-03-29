78535c3ced2d16c228558243705a5a8f1f11fc5a
nmusso <niccolo.mussoni@gmail.com>
2022-03-29 21:28:07 +0200

    Update javadoc

--- src/main/java/model/buffer/Buffer.java
+++ src/main/java/model/buffer/Buffer.java
@@ -7,19 +7,19 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 
 public interface Buffer {
     /**
-     * 
-     * @return The ID of the buffer
+     * Get the ID of the buffer.
+     * @return ID of the buffer
      */
     int getID();
 
     /**
-     * 
+     * Get the path of the file.
      * @return The path of the file from which the buffer was generated
      */
     String getFile();
 
     /**
-     * 
+     * Generate the buffer from the path passed in the constructor.
      * @return The ID of the generated buffer
      * @throws FileNotFoundException
      * @throws UnsupportedAudioFileException
--- src/main/java/model/buffer/BufferFactory.java
+++ src/main/java/model/buffer/BufferFactory.java
@@ -7,7 +7,7 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 
 public interface BufferFactory {
     /**
-     * 
+     * Create a new buffer if it was never generated from the path passed as parameter, otherwise gets the ID from the cache.
      * @param file The path of the file which will be associated to the buffer
      * @return The ID of the created buffer
      * @throws FileNotFoundException
