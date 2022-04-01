784d9cce07b3ef0598f40b074f57807855bbd8d9
Simone Lugaresi <simone.lugaresi@studio.unibo.it>
2022-04-01 18:20:48 +0200

    fix some warnings in mainView

--- src/main/java/view/MainView.java
+++ src/main/java/view/MainView.java
@@ -1,61 +1,32 @@
 package view;
 
-import javafx.animation.Animation;
-import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.geometry.Orientation;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Slider;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-import java.io.File;
-import java.util.ArrayList;
-
-
 public class MainView extends Application {
 
-	private float horizontal;
-	private float vertical;
-	private float maxX = 500;
-	private float maxY = 500;
-
-
-	/**
-	 *
-	 */
-	@Override
-	public void start(Stage mainStage) throws Exception {
-		mainStage.setTitle("Collector");
-		Parent root = FXMLLoader.load(getClass().getResource("provafxml.fxml"));
-		Scene scene = new Scene(root);
-		Button btnsource = (Button) scene.lookup("#addSource");
-		btnsource.setOnAction((ActionEvent event) -> {
-                  System.out.println("add Source");
-                });
-	        //set transparent
-	        scene.setFill(Color.TRANSPARENT);
-	        mainStage.setScene(scene);
-	        mainStage.show();
-	}
-	
-	public void reserchfolderClick() {
-		System.out.println("prova");
-	}
-
+    /**
+     * 
+     * {@inheritDoc}
+     */
+    @Override
+    public void start(final Stage mainStage) throws Exception {
+        mainStage.setTitle("Collector");
+        final Parent root = FXMLLoader.load(getClass().getResource("provafxml.fxml"));
+        final Scene scene = new Scene(root);
+        final Button btnsource = (Button) scene.lookup("#addSource");
+        btnsource.setOnAction((ActionEvent event) -> {
+            System.out.println("add Source");
+        });
+        // set transparent
+        scene.setFill(Color.TRANSPARENT);
+        mainStage.setScene(scene);
+        mainStage.show();
+    }
 }
--- src/main/java/view/ProvaMain.java
+++ src/main/java/view/ProvaMain.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 
 public class ProvaMain {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         Application.launch(MainView.class);
     }
 
