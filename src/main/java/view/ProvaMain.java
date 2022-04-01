54becae3be2805a04b47d9f367e4d7fb974149ad
Simone Lugaresi <simone.lugaresi@studio.unibo.it>
2022-04-01 17:44:39 +0200

    main View Test

---
+++ src/main/java/view/ProvaMain.java
@@ -0,0 +1,11 @@
+package view;
+
+import javafx.application.Application;
+
+public class ProvaMain {
+
+    public static void main(String[] args) {
+        Application.launch(mainView.class);
+    }
+
+}
