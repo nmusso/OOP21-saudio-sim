4a28ea9c670fc15123a56850dd6daf6c8ae5a06c
Simone Lugaresi <simone.lugaresi@studio.unibo.it>
2022-04-06 17:24:26 +0200

    add Envirnment.fxml

--- src/main/java/controller/EnvironmentController.java
+++ src/main/java/controller/EnvironmentController.java
@@ -3,20 +3,14 @@ package controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import javafx.event.Event;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
-public class Test1 implements Initializable {
+public class EnvironmentController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         // TODO Auto-generated method stub
         
-    }
-    
-    @FXML public final void handleTest1btn(final Event event) {
-        System.out.println("test primo controller");
     }
 
 }
