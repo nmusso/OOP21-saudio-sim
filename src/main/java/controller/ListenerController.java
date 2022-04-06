f49430bdfef25439b5578e84889304330f882bee
alex presepi <alex.presepi@studio.unibo.it>
2022-04-06 19:15:38 +0200

    Test multiple tab on Listener Pane

--- src/main/java/controller/ListenerController.java
+++ src/main/java/controller/ListenerController.java
@@ -1,26 +1,49 @@
 package controller;
 
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SplitMenuButton;
+import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
-import javafx.scene.layout.Pane;
+import view.utility.PageLoader;
 
 public class ListenerController implements Initializable {
     @FXML private Button btn;
     @FXML private TabPane listenerPane;
+    @FXML private MenuItem it;
+    @FXML private SplitMenuButton splitMenuPlugin;
 
     @Override
     public void initialize(final URL location, final ResourceBundle resources) {
+        /*TODO con ClassGraph ricercare tutte le classi dei plugin ed aggiungerle in splitMenuPlugin*/
+        EventHandler<ActionEvent> eh = (x) -> { 
+            System.out.println("PluginX"  + x.getSource());
+            };
+        MenuItem item = new MenuItem("PluginX");
+        item.setOnAction(eh);
+        splitMenuPlugin.getItems().add(item);
+
+
     }
-    
+
     @FXML public final void handleAddPlugin(final Event event) {
-        System.out.println(event.getSource());
+
+    }
+
+    @FXML public final void handleSelectPlugin(final Event event) {
+        final Tab drp = PageLoader.getPage("src/main/resources/fxml/DropplerPlugin.fxml");
+        MenuItem item = (MenuItem) event.getSource();
+        item.setDisable(true);
+        listenerPane.getTabs().add(drp);
     }
 
 }
