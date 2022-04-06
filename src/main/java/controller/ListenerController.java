635efc1cc7f2547302bdf042c982f19cb92efcb7
alex presepi <alex.presepi@studio.unibo.it>
2022-04-06 17:05:45 +0200

    Add view and controller Listener

--- src/main/java/controller/ListenerController.java
+++ src/main/java/controller/ListenerController.java
@@ -3,19 +3,24 @@ package controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.TabPane;
 import javafx.scene.layout.Pane;
 
 public class ListenerController implements Initializable {
     @FXML private Button btn;
-    @FXML private Pane pane;
+    @FXML private TabPane listenerPane;
 
     @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    public void initialize(final URL location, final ResourceBundle resources) {
     }
     
-    
+    @FXML public final void handleAddPlugin(final Event event) {
+        System.out.println(event.getSource());
+    }
+
 }
