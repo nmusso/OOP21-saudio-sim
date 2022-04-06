cc47001f9e5d1cb5e5bb164ee226d3c2e1c4639c
nmusso <niccolo.mussoni@gmail.com>
2022-04-06 17:37:46 +0200

    Fix Equalizer fxml and nearly completed controller

--- src/main/java/controller/EqualizerController.java
+++ src/main/java/controller/EqualizerController.java
@@ -1,6 +1,8 @@
 package controller;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
 
@@ -10,30 +12,45 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Slider;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import model.effect.ALEffects;
 
 public class EqualizerController implements Initializable {
 
-    @FXML private VBox container;
+    @FXML private GridPane slidersPane;
     @FXML private ToggleButton btnTurn;
 
     @Override
     public final void initialize(final URL location, final ResourceBundle resources) {
-        for (final Node node : container.getChildren()) {
-            if (node instanceof Slider) {
-                final Slider slider = (Slider) node;
-                final Optional<ALEffects> effect = getEffect(slider.getId());
-                if (effect.isPresent()) {
-                    final var eff = effect.get();
-                    slider.setMin(eff.getMinValue());
-                    slider.setMax(eff.getMaxValue());
-                    slider.valueProperty().addListener((obs, old, newValue) -> {
-                        //MainController.getSources().foreach(s -> s.applyFilter(s, eff, newValue.floatValue()));
-                    });
-                }
+        for (final Slider slider : getSliders()) {
+            final Optional<ALEffects> effect = getEffect(slider.getId());
+            if (effect.isPresent()) {
+                final var eff = effect.get();
+                slider.setMin(eff.getMinValue());
+                slider.setMax(eff.getMaxValue());
+                slider.valueProperty().addListener((obs, old, newValue) -> {
+                    // MainController.getSources().foreach(s -> s.applyFilter(s, eff,
+                    // newValue.floatValue()));
+                });
             }
         }
+    }
+
+    @FXML public final void handlePress(final Event event) {
+        final boolean state = btnTurn.isSelected();
+
+        if (!state) {
+            btnTurn.setText("Turn on");
+            btnTurn.setStyle("-fx-background-color: #F06249");
+        } else {
+            btnTurn.setText("Turn off");
+            btnTurn.setStyle("-fx-background-color: #6CF049");
+        }
+
+        getSliders().forEach(s -> s.setDisable(!state));
     }
 
     private Optional<ALEffects> getEffect(final String id) {
@@ -55,15 +72,14 @@ public class EqualizerController implements Initializable {
         }
     }
 
-    @FXML public final void handlePress(final Event event) {
-        final boolean state = btnTurn.isSelected();
+    private List<Slider> getSliders() {
+        final List<Slider> sliders = new ArrayList<>();
 
-        if (state) {
-            
-        } else {
-            
-        }
-
-        btnTurn.setSelected(!state);
+        slidersPane.getChildren().forEach(s -> {
+            if (s instanceof Slider) {
+                sliders.add((Slider) s);
+            }
+        });
+        return sliders;
     }
 }
