01c59609e079d7ceeeb8db0d90bcd07c4c9d0fee
alex presepi <alex.presepi@studio.unibo.it>
2022-04-08 00:34:47 +0200

    Fix return type of getPage in PageLoader utility

--- src/main/java/view/utility/PageLoader.java
+++ src/main/java/view/utility/PageLoader.java
@@ -2,24 +2,30 @@ package view.utility;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.Optional;
 
 import javafx.fxml.FXMLLoader;
-import javafx.scene.layout.Pane;
+import model.utility.Pair;
 
-public class PageLoader {
+public final class PageLoader {
 
+    private PageLoader() { }
     /**
     *
     */
-    public static <E> E getPage(final String fileName) {
-        E element = null;
+    public static <E, C> Optional<Pair<E, C>> getPage(final String fileName) {
+        E element;
+        C ctr;
+
         final FXMLLoader loader = new FXMLLoader();
         try {
              element = loader.load(new FileInputStream(new File(fileName)));
+             ctr = loader.getController();
         } catch (Exception e1) {
             // TODO Auto-generated catch block
             e1.printStackTrace();
+            return Optional.empty();
         } 
-        return element;
+        return Optional.of(new Pair<>(element, ctr));
     }
 }
