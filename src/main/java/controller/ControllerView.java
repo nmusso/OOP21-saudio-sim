7ddd741fb4cdb9ef32092339e024f1a45bc8fbdb
alex presepi <alex.presepi@studio.unibo.it>
2022-04-08 00:32:25 +0200

    Create utility Pair and interface ControllerView

---
+++ src/main/java/controller/ControllerView.java
@@ -0,0 +1,10 @@
+package controller;
+
+public interface ControllerView {
+
+    /**
+     * 
+     * @param ctrMain
+     */
+    void setControllerApplication(MainControllerApplication ctrMain);
+}
