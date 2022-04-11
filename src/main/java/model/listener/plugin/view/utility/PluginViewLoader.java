4aecceebb4b39fc22a2f4e0435d309af42d8e598
alex presepi <alex.presepi@studio.unibo.it>
2022-04-12 00:09:19 +0200

    Add call to PageLoader in PluginViewLoader

--- src/main/java/model/listener/plugin/view/utility/PluginViewLoader.java
+++ src/main/java/model/listener/plugin/view/utility/PluginViewLoader.java
@@ -4,13 +4,14 @@ import java.util.Optional;
 
 import javafx.scene.control.Tab;
 import model.utility.Pair;
+import view.utility.PageLoader;
 
 
 public final class PluginViewLoader {
     private PluginViewLoader() { }
 
     public static <C> Optional<C> tabPluginLoader(final String fileName) {
-        final Optional<Pair<Tab, C>> tabInfo = Optional.empty(); //PageLoader.getPage(fileName);
+        final Optional<Pair<Tab, C>> tabInfo = PageLoader.getPage(fileName);
         return tabInfo.isPresent() ? Optional.of(tabInfo.get().getY()) : Optional.empty();
 
     }
