268cefda729d10e230da94e2efd8ddd57eb3ee0f
nmusso <niccolo.mussoni@gmail.com>
2022-04-21 17:24:06 +0200

    Fix build gradle for LWJGL

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,27 +33,14 @@ val javaFXModules = listOf("base", "controls", "fxml", "swing", "graphics")
 val supportedPlatforms = listOf("linux", "mac", "win") // All required for OOP
 val javaFxVersion = 15
 val lwjglVersion = "3.3.1"
-val lwjglNatives = Pair(
-	System.getProperty("os.name")!!,
-	System.getProperty("os.arch")!!
-).let { (name, arch) ->
-	when {
-		arrayOf("Linux", "FreeBSD", "SunOS", "Unit").any { name.startsWith(it) } ->
-			if (arrayOf("arm", "aarch64").any { arch.startsWith(it) })
-				"natives-linux${if (arch.contains("64") || arch.startsWith("armv8")) "-arm64" else "-arm32"}"
-			else
-				"natives-linux"
-		arrayOf("Mac OS X", "Darwin").any { name.startsWith(it) }                ->
-			"natives-macos${if (arch.startsWith("aarch64")) "-arm64" else ""}"
-		arrayOf("Windows").any { name.startsWith(it) }                           ->
-			if (arch.contains("64"))
-				"natives-windows${if (arch.startsWith("aarch64")) "-arm64" else ""}"
-			else
-				"natives-windows-x86"
-		else -> throw Error("Unrecognized or unsupported platform. Please set \"lwjglNatives\" manually")
-	}
-}
-
+val lwjglPlatforms = listOf("natives-linux",
+							"natives-linux-arm64",
+							"natives-linux-arm32",
+							"natives-macos",
+							"natives-macos-arm64",
+							"natives-windows",
+							"natives-windows-x86",
+							"natives-windows-arm64")
 
 dependencies {
     // Maven dependencies are composed by a group name, a name and a version, separated by colons
@@ -80,12 +67,15 @@ dependencies {
 	implementation("org.lwjgl", "lwjgl-openal")
 	implementation("org.lwjgl", "lwjgl-opengl")
 	implementation("org.lwjgl", "lwjgl-stb")
-	runtimeOnly("org.lwjgl", "lwjgl", classifier = lwjglNatives)
-	runtimeOnly("org.lwjgl", "lwjgl-assimp", classifier = lwjglNatives)
-	runtimeOnly("org.lwjgl", "lwjgl-glfw", classifier = lwjglNatives)
-	runtimeOnly("org.lwjgl", "lwjgl-openal", classifier = lwjglNatives)
-	runtimeOnly("org.lwjgl", "lwjgl-opengl", classifier = lwjglNatives)
-	runtimeOnly("org.lwjgl", "lwjgl-stb", classifier = lwjglNatives)
+	
+	for (platform in lwjglPlatforms) {
+		runtimeOnly("org.lwjgl", "lwjgl", classifier = platform)
+		runtimeOnly("org.lwjgl", "lwjgl-assimp", classifier = platform)
+		runtimeOnly("org.lwjgl", "lwjgl-glfw", classifier = platform)
+		runtimeOnly("org.lwjgl", "lwjgl-openal", classifier = platform)
+		runtimeOnly("org.lwjgl", "lwjgl-opengl", classifier = platform)
+		runtimeOnly("org.lwjgl", "lwjgl-stb", classifier = platform)
+	}
 	
 	//ClassGraph
 	implementation("io.github.classgraph:classgraph:4.8.143")
